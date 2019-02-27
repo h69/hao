8ee66f37efd307833de3b9219d64a7897b332b8e
adamearthhuang <517364792@qq.com>
2019-02-28 02:13:04 +0800

    fix

--- fe/config/index.js
+++ fe/config/index.js
@@ -38,12 +38,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    // index: path.resolve(__dirname, '../dist/index.html'),
-    index: path.resolve(__dirname, '../../web/views/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    // assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsRoot: path.resolve(__dirname, '../../web/public'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
